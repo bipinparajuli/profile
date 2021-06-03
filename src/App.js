cbeedcdaccf437cb831b1d51a5f3afd1d2f44a40
bipinparajuli <bipinprjl@gmail.com>
2021-06-03 18:32:15 +0545

    home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import {BrowserRouter,Route,Switch} from "react-router-dom"
 import Home from "./Component/Home"
+import About from "./Component/About"
+import PageNotFound from "./Component/PageNotFound"
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+
+
+        <Route exact component={PageNotFound} />
+
       </Switch>
       </BrowserRouter>
     </div>
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -9,10 +9,12 @@ const Home = () => {
         <Container fluid className="home">
             <Row style={{width:"100%",justifyContent:"center",alignItems:"center"}}>
                 <Col style={{color:"white"}}>
-                <h1 className="firstheading">hello .<span className="one" >About</span></h1> 
-                <h1 style={{color:"#5A20CB"}} className="secondheading">I <span className="two">Contact</span></h1>
-                <h1 style={{color:"#5A20CB"}} className="thirdheading">am <span className="three">Blog</span></h1>
-                <h1 style={{color:"#5A20CB"}} className="fourthheading">Bipin <span className="four">Projects</span></h1>
+               <Link to="/about">
+               <h1 className="firstheading"></h1>
+               </Link>  
+                <h1 style={{color:"#5A20CB"}} className="secondheading"></h1>
+                <h1 style={{color:"#5A20CB"}} className="thirdheading"></h1>
+                <h1 style={{color:"#5A20CB"}} className="fourthheading"></h1>
 
                 </Col>
                 <Col className="second_col">
