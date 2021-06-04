c18829dda2c910bbf46b47d9af4506d4b7522f01
bipinparajuli <bipinprjl@gmail.com>
2021-06-04 12:00:42 +0545

    home complete

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,25 +1,46 @@
 import React from 'react'
 import "./Home.css"
-import image from '../assets/my-img.png'
-import {Image,Container,Row,Col} from "react-bootstrap"
+import {Container,Row,Col} from "react-bootstrap"
 import {Link} from "react-router-dom"
+import {Fade,Zoom} from "react-reveal"
 
 const Home = () => {
     return (
         <Container fluid className="home">
             <Row style={{width:"100%",justifyContent:"center",alignItems:"center"}}>
                 <Col style={{color:"white"}}>
-               <Link to="/about">
-               <h1 className="firstheading"></h1>
-               </Link>  
-                <h1 style={{color:"#5A20CB"}} className="secondheading"></h1>
-                <h1 style={{color:"#5A20CB"}} className="thirdheading"></h1>
-                <h1 style={{color:"#5A20CB"}} className="fourthheading"></h1>
+                  <Fade bottom>
+
+                        <Link to="/about">
+                                    <h1 className="firstheading"  data-aos="fade-up"  data-aos-duration="100"  ></h1>
+                        </Link>  
+                    </Fade>
+
+                    <Fade bottom>
+                        <Link to="/contact">
+
+                            <h1 style={{color:"#5A20CB"}} className="secondheading"></h1>
+                        </Link>
+                    </Fade>
+
+                    <Fade bottom>
+                        <Link to="/blogs">
+                            <h1 style={{color:"#5A20CB"}} className="thirdheading"></h1>
+                        </Link>
+                    </Fade>
+
+                    <Fade bottom>
+                        <Link to="/projects">
+                            <h1 style={{color:"#5A20CB"}} className="fourthheading"></h1>
+                        </Link>
+                    </Fade>
+                
 
                 </Col>
                 <Col className="second_col">
-                
-                </Col>
+                    
+                    </Col>
+                    
             </Row>
         </Container>
     )
